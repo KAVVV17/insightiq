c896a93a0d1d2923b6b9f9e9c41ef9e1fd7e8342
Kavana Ambekar <kavvv.anna317@gmail.com>
2026-05-19 23:48:52 +0530

    fixed render port

--- backend/app.py
+++ backend/app.py
@@ -121,7 +121,7 @@ def feedback():
     return jsonify({
         "message": f"Feedback received! Sentiment: {sentiment}"
     })
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
